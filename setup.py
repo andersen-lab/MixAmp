130c74d52e3cf71c583984e538de78a7d60f9d8a
Maryam <mary.elf97@gmail.com>
2025-02-26 13:40:55 -0800

    updating versions

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ description = ("Amplicon read simualtor")
 
 setup(
     name="mixamp",
-    version="2024.11",
+    version="2025.02",
     packages=find_packages(include=['mixamp']),
     author="Maryam Ahmadi Jeshvaghane",
     license='BSD 2-Clause',
